97c430d76bd22cc94203e601f2a9a557fe0631b4
gi8 <giottolino@gmail.com>
2023-12-16 09:56:30 +0100

    improve api token error message

--- internal/server/auth.go
+++ internal/server/auth.go
@@ -10,6 +10,14 @@ import (
 func ApiTokenMiddleware(next http.HandlerFunc, token string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		apiToken := r.Header.Get("X-API-Token")
+
+		if apiToken == "" {
+			msg := "Missing API token. You must provide the API token in the X-API-Token header."
+			log.Error().Msg(msg)
+			http.Error(w, msg, http.StatusUnauthorized)
+			return
+		}
+
 		if apiToken != token {
 			log.Error().Msgf("Unauthorized API token: %s", apiToken)
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
